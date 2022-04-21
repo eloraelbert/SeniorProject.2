cc2822fa5389edb96226c6b6dd442da44cbc171d
Elora Elbert <82468024+eloraelbert@users.noreply.github.com>
2022-04-21 22:27:41 +0000

    add placeholder instead of value

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 
 
 function createNewElement() {
-  // First create a DIV element.
-	var txtNewInputBox = document.createElement('div');
-  txtNewInputBox.classList.add("draggable");
-  //set divCount to # of draggable divs
   var divcount = document.querySelectorAll('.draggable').length;
+  // First create a DIV element.
+	var txtNewInputBox = document.createElement('input');
+  txtNewInputBox.setAttribute('type', 'text');
+  txtNewInputBox.setAttribute('draggable', 'true');
+  txtNewInputBox.setAttribute('id', 'drag'+ divcount);
+  txtNewInputBox.setAttribute('ondragstart', 'drag(event)');
+  txtNewInputBox.setAttribute('name', 'fname');
+  txtNewInputBox.setAttribute('placeholder', 'Create Element');
+  txtNewInputBox.setAttribute('ondblclick', 'deleteBox()');
+  
+  //txtNewInputBox.classList.add("draggable");
+  //set divCount to # of draggable divs
 
   // Then add the content (a new input box) of the element.
-	txtNewInputBox.innerHTML = "<input draggable = 'true' type='text' id='drag" + 
-    divcount + "' ondragstart='drag(event)' type='text' id='fname' name='fname' value='Create Element' ondblclick='deleteBox()'>";
+	// txtNewInputBox.innerHTML = "<input draggable = 'true' type='text' id='drag" + 
+ //    divcount + "' ondragstart='drag(event)' type='text' id='fname' name='fname' value='Create Element' ondblclick='deleteBox()'>";
 
   // Finally put it where it is supposed to appear.
 	document.getElementById("inputBoard").appendChild(txtNewInputBox);
